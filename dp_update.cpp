58b79162a486ed5916bb69b2ab62bb6ae0b27525
Stephen Agee <sagee@mines.edu>
2018-03-19 22:44:46 -0600

    Recursive algorithm is working and now just needs DP component

--- dp_update.cpp
+++ dp_update.cpp
@@ -4,66 +4,80 @@ using namespace std;
 
 struct Config {
 	int n, L, P, c;
-	int g[];
+	Config(){};
 };
 
-int orderGas(vector<vector <int> > & dpTable, int* g, Config config){
+struct Entry {
+	int cost;
+	int nextDay;
+	int stored;
+};
+Entry dpTable [100][100];
 
-	for (int i = 0; i < config.n; i++){
-		cout << g[i] << " ";
-	}
-	cout << endl;
+int orderGas(int* g, Config config, int gasStored, int day){
 
+//cout << "Day: " << day << " Gas Stored: " << gasStored << endl;
 
-/*
-	//if we already have the cost stored in the table, return it
-	if (dpTable[day][gallons] != -1){
-		cost += dpTable[][];
-	}
-	else {
-		gallonsOrdered += 1;
-		if (gallonsOrdered > 0){
-			cost += 
-		}
-	}
+	//initially, we add the night before's storage to the cost
+	int cost = gasStored*config.c;
+	//cout << gasStored << "con " << config.c << endl;
+	//cout << "first: " << cost << endl;
 
-	//base case - if we reach the end of all of the days
-	if (currentDay == TOTAL_DAYS - 1){
-		//if we have gas stored
-		if (storedGas != 0){
-			//return a very large number, ideally infinity
+	//base case - last day
+	if (day == config.n - 1){
+		//if the tank is not empty, return [ideally] inf, because it is an invalid cost
+		if ((gasStored - g[day]) > 0){
 			return 10000;
 		}
-		//otherwise, this is a valid solution
-		return cost;
+		//otherwise, return cost of ordered gas
+		//if we have exactly the right amount of gas
+		if ((gasStored - g[day]) == 0){
+			return cost;
+		}
+		//otherwise, we'll need to order some gas
+		return (cost + config.P);
 	}
 
 	
-	else if (gallonsOrdered < 0){
-		return orderGas(dpTable, n, g, day, gallonsOrdered + 1, cost);
+	//then subtract the gas we need from the gas stored
+	int gasNeeded = g[day] - gasStored;
+	if (gasNeeded > 0){
+		//we need to order gas on this day...pay for delivery
+		cost += config.P;
+		//cout << cost << endl;
 	}
-*/
+	else if (gasNeeded < 0) gasNeeded = 0;
 
-	return 0;
-
-
+	int minCost = 10000;
+	//for 0 extra gallons to the maximum capacity, order gas for the day
+	for (int i = gasNeeded; i <= (config.L - gasStored + g[day]); i++){
+		int tempCost = cost + orderGas(g, config, gasStored + i - g[day], day + 1);
+		if (tempCost < minCost){
+			minCost = tempCost;
+		}
+	}
+	return minCost;
 }
 
 int main(){
 	//first we define all of the given constants
 	Config config;
-	config.n = 15;
+	//fix this too
+	config.n = 2;
 	config.L = 6;
 	config.P = 17;
 	config.c = 4;
 	//g = gas needed for each of the reamining days
-	int g[] = {4,3,1,4,2,2,4,3,1,6,6,1,5,5,5};
+	//int g[] = {4,3,1,4,2,2,4,3,1,6,6,1,5,5,5};
+	int g[] = {4,3};
 
 	//initialize the dp table with -1 in every location
-	vector < vector<int> > dpTable (config.n, vector<int> (config.n, -1));
+	
 	int minimumCost = -1;
 
-	minimumCost = orderGas(dpTable, g, config);
+	minimumCost = orderGas(g, config, 0, 0);
+
+	cout << minimumCost << endl;
 
 	return 0;
 }
